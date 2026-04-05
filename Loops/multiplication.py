12ceb9bc212d4f125bc3570237717cf42c54e25a
satish210193 <satish210193@gmail.com>
2026-04-05 16:00:44 +0530

    xyz

---
+++ Loops/multiplication.py
@@ -0,0 +1,18 @@
+
+# ##Multiplication table of a number using while loop
+# n = 1
+# no = int(input("Enter a number to get the table: "))
+# print(f"multipcation of a {no}")
+# while n<=10:
+#     print(f"{no} * {n} = {no*n}")
+#     n += 1
+    
+    
+
+## Using for loop
+no = int(input("Enter a number to get the table: "))
+print(f"multipcation of a {no}")
+for i in range(1, 11):
+    print(f"{no} * {i} = {no * i}")
+
+xyz
